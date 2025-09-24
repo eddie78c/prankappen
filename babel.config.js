6ca0fdc1201ce43a5e06a9adf09a7070dfb40f93
eddie78c <eddie.hbg@gmail.com>
2025-09-25 01:19:05 +0200

    Remove globals option from react-native-reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['react-native-reanimated/plugin', {
-        globals: ['__scanAnimatedProps', '__scanAnimatedStyle']
-      }]
+      'react-native-reanimated/plugin'
     ],
   };
 };
